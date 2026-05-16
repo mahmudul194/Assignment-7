2079124a1d8484e480e5286011369ed64b1ccb22
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-05-16 20:39:36 +0600

    added toaster msg for call, text and video call

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router'
 import './App.css'
+import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
@@ -14,6 +15,7 @@ function App() {
           <Outlet/>
         </main>
         <Footer/>
+        <Toaster position="bottom-right" reverseOrder={false} />
       </div>
     </>
   )
