24d50e1688b9ec092c68a0b6e734b47a47094eb4
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-05-16 19:55:54 +0600

    added outlet for mpa

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router'
 import './App.css'
 import Navbar from './components/Navbar'
 
@@ -8,6 +9,9 @@ function App() {
     <>
       <div className="min-h-screen flex flex-col bg-[#f8fafc]">
         <Navbar/>
+        <main className='grow'>
+          <Outlet/>
+        </main>
       </div>
     </>
   )
