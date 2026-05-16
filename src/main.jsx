ff0dfb408eeb6d965ace6ac58365da115b80504a
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-05-16 20:06:06 +0600

    added the app path and the home path to the main.jsx file and created router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from './pages/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    Component:App,
+    children:[
+      {
+        path:'/',
+        Component:Home
+      },
+      
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
